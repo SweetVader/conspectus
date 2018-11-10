705e8737a567b2be95b7d1b92f6983e1ac897e5f
NastassiaSh <xxxnastikxxx@mail.ru>
2018-11-10 15:17:20 +0300

    Create Controller, Application file and greeting page

---
+++ conspect/src/main/java/com/example/conspect/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.conspect;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="Sweetie") String name,
+            Model model
+    ) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
