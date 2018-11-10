087fd468113a61feddd0e1322f40f611192fffab
NastassiaSh <xxxnastikxxx@mail.ru>
2018-11-10 18:09:42 +0300

    Add filter

--- conspect/src/main/java/com/example/conspect/repos/MessageRepo.java
+++ conspect/src/main/java/com/example/conspect/repos/MessageRepo.java
@@ -3,5 +3,9 @@ package com.example.conspect.repos;
 import com.example.conspect.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface MessageRepo extends CrudRepository<Message, Long> {
+
+    List<Message> findByTag(String tag);
 }
