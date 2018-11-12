adc512541840957bd90d01d7e1ea32ce1961e7c3
NastassiaSh <xxxnastikxxx@mail.ru>
2018-11-12 11:04:32 +0300

    Add author for message. Add links between tables

--- conspect/src/main/java/com/example/conspect/controller/MainController.java
+++ conspect/src/main/java/com/example/conspect/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.conspect.controller;
 
 import com.example.conspect.domain.Message;
+import com.example.conspect.domain.User;
 import com.example.conspect.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,8 +31,11 @@ public class MainController {
         return "main";
     }
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String , Object> model) {
-       Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String , Object> model) {
+       Message message = new Message(text, tag, user);
 
        messageRepo.save(message);
        Iterable<Message> messages = messageRepo.findAll();
