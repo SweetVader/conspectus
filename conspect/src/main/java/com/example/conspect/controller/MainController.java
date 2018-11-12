c92644feb2e74c9f15be8015ce32afa32a05bd59
NastassiaSh <xxxnastikxxx@mail.ru>
2018-11-12 12:05:39 +0300

    Replace mustache on freemarker

--- conspect/src/main/java/com/example/conspect/controller/MainController.java
+++ conspect/src/main/java/com/example/conspect/controller/MainController.java
@@ -23,13 +23,24 @@ public class MainController {
     public String greeting(Map<String, Object> model) {
         return "greeting";
     }
+
     @GetMapping("/main")
-    public String main(Map<String , Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
+
     @PostMapping("/main")
     public String add(
             @AuthenticationPrincipal User user,
@@ -43,20 +54,4 @@ public class MainController {
 
         return "main";
     }
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String , Object> model){
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-
-        }
-        model.put("messages", messages);
-
-        return "main";
-    }
-
-
 }
